97f2c7fde8a815d9776028d38d276c7bf4125766
Tyler Wang <46231885+gucciwang123@users.noreply.github.com>
2023-12-29 01:26:55 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Template 
+
+Template for CMake Projects
+
+## Build
+
+This project uses CMake to manage dependencies and build so you must have CMake and GNU Make installed.
+
+1. `mkdir build`
+2. `cd build`
+3. `cmake ..`
+4. `make`
+
---
+++ src/include/pch.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef uint8_t EP_BYTE;
+typedef uint64_t EP_QWORD;
+
+typedef double EP_QWORD_F;
+
+typedef EP_BYTE EP_BOOL;
+#define EP_FALSE 0
+#define EP_TRUE 1
+
+
+typedef int16_t EP_ERROR;
+
+#define EP_SUCCESS 0
